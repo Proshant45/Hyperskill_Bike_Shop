6c70a8816c75de61c271e82f9c1dbf93cb982fcc
prosanto <proshantbhakta@gmail.com>
2024-10-16 15:45:17 +0600

    Initial commit

---
+++ Bike Shop/task/shop/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from shop.models import *
+admin.site.register(Frame)
+admin.site.register(Seat)
+admin.site.register(Basket)
+admin.site.register(Tire)
+admin.site.register(Bike)
+admin.site.register(Order)
